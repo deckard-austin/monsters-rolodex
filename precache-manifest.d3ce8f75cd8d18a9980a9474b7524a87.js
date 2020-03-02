2984d54fd2fd69b73c76b2ae3519c6cedeedd258
deckard-austin <deckardaustin852@gmail.com>
2020-03-02 16:10:01 +1100

    Updates

---
+++ precache-manifest.d3ce8f75cd8d18a9980a9474b7524a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3cab16b338c8b954947b76c967ce74",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "493074db7378e9fca699",
+    "url": "/monsters-rolodex/static/css/main.6e6d98c3.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493074db7378e9fca699",
+    "url": "/monsters-rolodex/static/js/main.4962ed9a.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
